937779028743a493b219cc66570dab70370f558b
RoyalFlushever <royalflushdiamond918@gmail.com>
2018-03-19 08:43:22 -0400

    proper header check

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -3,25 +3,9 @@ class Inventory < ApplicationRecord
 
     inventories_json = []
 		CSV.foreach(file.path, headers: true, :encoding => Encoding::ISO_8859_1) do | row |
-   #    t = Inventory.new
-			# t.name = row['item-name'] 
-   #    t.listingID = row['listing-id']
-   #    t.sellerSku = row['seller-sku']
-   #    t.price = row['price']
-   #    t.quantity = row['quantity']
-   #    t.opendate = row['open-date']
-   #    t.imageUrl = row['image-url']
-   #    t.isMarketplace = row['item-is-marketplace']
-   #    t.asin1 = row['asin1']
-   #    t.asin2 = row['asin2']
-   #    t.asin3 = row['asin3']
       # CSV Header Check
       # 
-      headers = ["item-name", "item-description", "listing-id", "seller-sku", "price", "quantity", "open-date", "item-is-marketplace", "product-id-type", "zshop-shipping-fee", "item-note", "item-condition", "zshop-category1", "zshop-browse-path", "zshop-storefront-feature", "asin1", "asin2", "asin3", "will-ship-internationally", "expedited-shipping", "zshop-boldface", "product-id", "bid-for-featured-placement", "add-delete", "pending-quantity", "fulfillment-channel"]
       headers = ["item-name", "listing-id", "seller-sku", "price", "quantity", "open-date", "item-is-marketplace", "asin1", ]
-      p row.headers
-      # return "Check your CSV file Columns!" unless row.headers.sort == headers.sort
-
       headers.each do |header|
         return "Check your CSV file Columns!" unless row.headers.include? header
       end
