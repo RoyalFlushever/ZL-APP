24466a48926658c381df5c5a87241cc32af1ace1
RoyalFlushever <royalflushdiamond918@gmail.com>
2018-03-24 10:45:03 -0400

    mail_to help

--- app/controllers/upload_controller.rb
+++ app/controllers/upload_controller.rb
@@ -12,11 +12,11 @@ class UploadController < ApplicationController
 
 		if params[:file].blank? || params[:file].path.empty?
 
-      flash[:error] = "Warning! You must select File first."
-      redirect_to upload_path
-      return
+			flash[:error] = "Warning! You must select File first."
+			redirect_to upload_path
+			return
 
-    else
+		else
 
 			file_name = Inventory.import_from_csv(params[:file])
 			
@@ -32,7 +32,7 @@ class UploadController < ApplicationController
 			Progressbar.init(file_name)
 
 			respond_to do |format|
-		    result = { redirect_url: start_url, filename: file_name }
+				result = { redirect_url: start_url, filename: file_name }
 		    format.json  { render :json => result } # don't do msg.to_json
 		  end
 
