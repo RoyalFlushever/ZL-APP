21b40637c6d492f012c3bdf32e6f53ab81660777
RoyalFlushever <royalflushdiamond918@gmail.com>
2018-03-26 23:39:43 -0400

    STATUS REMOVE

--- app/controllers/progress_controller.rb
+++ app/controllers/progress_controller.rb
@@ -1,5 +1,4 @@
 class ProgressController < ApplicationController
-	
 	# showing progress, here search product advertising API, bookfinder API
 	def index
 		filename = params[:filename]
@@ -10,11 +9,8 @@ class ProgressController < ApplicationController
 	# GET start
 	# GET start.json
 	def start
-
 		filename = params[:filename]
 		@progress_bar = Progressbar.find_by(taskname: filename)
 		render json: @progress_bar
-
 	end
-
 end
